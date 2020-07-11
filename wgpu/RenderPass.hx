e4b4af521e35c10a0330f087fe985378a86f3866
Valentin Lemière <valentin@ibilon.com>
2020-07-11 17:50:46 +0200

    API for RenderPass

--- wgpu/RenderPass.hx
+++ wgpu/RenderPass.hx
@@ -1,7 +1,5 @@
 package wgpu;
 
-// TODO missing functions: many
-
 /**
 	An in-progress recording of a render pass.
 **/
@@ -28,9 +26,69 @@ class RenderPass {
 		untyped __cpp__('wgpu_render_pass_draw_indexed(native, indicesEnd - indicesStart, instancesEnd - indicesStart, indicesStart, baseVertex, instancesStart)');
 	}
 
+	/**
+		Draws indexed primitives using the active index buffer and the active vertex buffers, based on the contents of the `indirectBuffer`.
+
+		The active index buffer can be set with `RenderPass.setIndexBuffer`, while the active vertex buffers can be set with `RenderPass.setVertexBuffers`.
+
+		@throws UseAfterDestroyException If `indirectBuffer` was already destroyed.
+	**/
+	public function drawIndexedIndirect(indirectBuffer:Buffer, indirectOffset:Int):Void {
+		indirectBuffer.validate();
+		untyped __cpp__('wgpu_render_pass_draw_indexed_indirect(native, indirectBuffer->native, indirectOffset)');
+	}
+
+	/**
+		Draws primitives from the active vertex buffer(s) based on the contents of the `indirectBuffer`.
+
+		The active vertex buffers can be set with `RenderPass.setVertexBuffers`.
+
+		@throws UseAfterDestroyException If `indirectBuffer` was already destroyed.
+	**/
+	public function drawIndirect(indirectBuffer:Buffer, indirectOffset:Int):Void {
+		indirectBuffer.validate();
+		untyped __cpp__('wgpu_render_pass_draw_indirect(native, indirectBuffer->native, indirectOffset)');
+	}
+
 	public function endPass():Void {
 		// TODO does this invalidate the instance?
 		untyped __cpp__('wgpu_render_pass_end_pass(native)');
+	}
+
+	/**
+		Sets the active bind group for a given bind group index.
+
+		@throws UseAfterDestroyException If `bindGroup` was already destroyed.
+	**/
+	public function setBindGroup(index:Int, bindGroup:BindGroup, offsets:Array<Int>):Void {
+		bindGroup.validate();
+
+		untyped __cpp__('
+			uint32_t *native_offsets = (uint32_t*)malloc(sizeof(*native_offsets) * offsets->length);
+
+			for (unsigned int i = 0; i < offsets->length; ++i) {
+				native_offsets[i] = (uint32_t)offsets->__get(i);
+			}
+
+			wgpu_render_pass_set_bind_group(native, index, bindGroup->native, native_offsets, offsets->length);
+
+			free(native_offsets);
+		');
+
+	}
+
+	public function setBlendColor(color:Color):Void {
+		untyped __cpp__('
+			WGPUColor native_color = {
+				r: color->r,
+				g: color->g,
+				b: color->b,
+				a: color->a,
+			};
+
+			wgpu_render_pass_set_blend_color(native, &native_color);
+		');
+
 	}
 
 	/**
@@ -57,6 +115,24 @@ class RenderPass {
 	}
 
 	/**
+		Sets the scissor region.
+
+		Subsequent draw calls will discard any fragments that fall outside this region.
+	**/
+	public function setScissorRect(x:Int, y:Int, width:Int, height:Int):Void {
+		untyped __cpp__('wgpu_render_pass_set_scissor_rect(native, x, y, width, height)');
+	}
+
+	/**
+		Sets the stencil reference.
+
+		Subsequent stencil tests will test against this value.
+	**/
+	public function setStencilReference(reference:Int):Void {
+		untyped __cpp__('wgpu_render_pass_set_stencil_reference(native, reference)');
+	}
+
+	/**
 		Assign a vertex buffer to a slot.
 
 		Subsequent calls to `draw` and `drawIndexed` on this `RenderPass` will use `buffer` as one of the source vertex buffers.
@@ -70,5 +146,14 @@ class RenderPass {
 	public function setVertexBuffer(slot:Int, buffer:Buffer, offset:Int, size:Int):Void {
 		buffer.validate();
 		untyped __cpp__('wgpu_render_pass_set_vertex_buffer(native, slot, buffer->native, offset, size)');
+	}
+
+	/**
+		Sets the viewport region.
+
+		Subsequent draw calls will draw any fragments in this region.
+	**/
+	public function setViewport(x:Float, y:Float, width:Float, height:Float, minDepth:Float, maxDepth:Float):Void {
+		untyped __cpp__('wgpu_render_pass_set_viewport(native, x, y, width, height, minDepth, maxDepth)');
 	}
 }
